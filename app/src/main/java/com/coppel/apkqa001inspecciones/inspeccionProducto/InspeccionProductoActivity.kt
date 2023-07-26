f920bfae203814cad9797de8d4efc748f8e5923f
julio.armijo <c354rzz@gmail.com>
2023-07-25 19:12:28 -0600

    upModeling_inspeccionProducto
    se genera el modelado de inspección producto

--- app/src/main/java/com/coppel/apkqa001inspecciones/inspeccionProducto/InspeccionProductoActivity.kt
+++ app/src/main/java/com/coppel/apkqa001inspecciones/inspeccionProducto/InspeccionProductoActivity.kt
@@ -2,6 +2,8 @@ package com.coppel.apkqa001inspecciones.inspeccionProducto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
 import androidx.activity.viewModels
 import com.coppel.apkqa001inspecciones.R
 import com.coppel.apkqa001inspecciones.databinding.ActivityInspeccionProductoBinding
@@ -9,7 +11,7 @@ import com.coppel.apkqa001inspecciones.inspeccionProducto.viewModel.InspeccionPr
 
 
 class InspeccionProductoActivity : AppCompatActivity() {
-    private lateinit var  binding: ActivityInspeccionProductoBinding
+    private lateinit var binding: ActivityInspeccionProductoBinding
     private val viewModel: InspeccionProductoViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,8 +21,52 @@ class InspeccionProductoActivity : AppCompatActivity() {
         supportActionBar?.title = getString(R.string.inspeccion_loads_name_app)
 
         val getProducto = intent.extras
-        if (getProducto != null){
-            binding.tvNombreProducto.text = "${getProducto.getString("sku") +" - "+ getProducto.getString("nombre")}"
+        if (getProducto != null) {
+            binding.tvNombreProducto.text =
+                "${getProducto.getString("sku") + " - " + getProducto.getString("nombre")}"
         }
     }
+
+    fun onRBClickedEmpaque(view: View) {
+        if (view is RadioButton) {
+            // Is the button now checked?
+            val checked = view.isChecked
+
+            // Check which radio button was clicked
+            when (view.getId()) {
+                R.id.rb_manual_si ->
+                    if (checked) {
+                        // opcion si
+                    }
+                R.id.rb_manual_no ->
+                    if (checked) {
+                        // opcion si
+                    }
+                R.id.rb_manual_na ->
+                    if (checked) {
+                        // opcion no aplica
+                    }
+            }
+        }
+    }
+
+    fun onRbClickedHallazgo(view: View) {
+        if (view is RadioButton) {
+            // Is the button now checked?
+            val checked = view.isChecked
+
+            // Check which radio button was clicked
+            when (view.getId()) {
+                R.id.rb_hallazgo_si ->
+                    if (checked) {
+                        // opcion si
+                    }
+                R.id.rb_hallazgo_no ->
+                    if (checked) {
+                        // opcion si
+                    }
+            }
+        }
+    }
+
 }
